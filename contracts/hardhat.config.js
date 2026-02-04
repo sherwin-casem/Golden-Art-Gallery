6cc514e826eabbcc0a1c0b679c655c6ff97a67c4
sherwin-casem <162948581+ad-larsen-co@users.noreply.github.com>
2026-02-04 19:08:33 +0100

    fix:fix to sepolia

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,10 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      gasPrice: "auto"
     }
   }
 };
